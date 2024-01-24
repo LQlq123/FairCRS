d49b38f5e750c05ab1ffdd83019c704771a531ac
Qin <117266923+LQlq123@users.noreply.github.com>
2024-01-24 18:45:57 +0800

    Delete evaluator directory

--- evaluator/metrics/__init__.py
+++
@@ -1,4 +0,0 @@
-from .base import Metric, Metrics, aggregate_unnamed_reports, AverageMetric
-from .gen import BleuMetric, ExactMatchMetric, F1Metric, DistMetric, EmbeddingAverage, VectorExtrema, \
-    GreedyMatch
-from .rec import HitMetric, NDCGMetric, MRRMetric
